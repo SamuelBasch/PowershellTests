566c63ba1621b88f265ea60339d01b89d12ecd17
SamuelBasch <123377201+SamuelBasch@users.noreply.github.com>
2023-02-01 15:51:09 -0500

    Create LogSID500.ps1

---
+++ LogSID500.ps1
@@ -0,0 +1,16 @@
+$cred = Get-Credential
+$servers = Get-Content "C:\temp\servers.txt"
+foreach ($server in $servers) {
+    Write-Host "Checking server $server"
+    $accounts = Get-WmiObject -Class Win32_UserAccount -ComputerName $server -Credential $cred
+    foreach ($account in $accounts) {
+        $username = $account.Name
+        $sid = $account.SID
+        $output = New-Object PSObject -Property @{
+            Server = $server
+            User = $username
+            SID = $sid
+        }
+        $output | Export-Csv -Path "C:\temp\output.csv" -NoTypeInformation -Append
+    }
+}
